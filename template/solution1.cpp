b0fa5da6db7e36992d20f14f0fd0dde0debc5d83
vonas <24623262+vonas@users.noreply.github.com>
2023-12-04 20:45:41 +0100

    Add solution template

---
+++ template/solution1.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+#include "solution1.h"
+
+using namespace dayX::solution1;
+
+void dayX::solution1::parse_line(std::string const& line)
+{
+}
+
+void dayX::solution1::parse_lines(std::vector<std::string> const& lines)
+{
+}
---
+++ template/test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+
+#include "solution1.h"
+#include "lib.h"
+
+using namespace dayX::solution1;
+using namespace testing;
+
+namespace dayX
+{
+}
