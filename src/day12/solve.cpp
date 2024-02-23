0d6b2825bbf3c5916bd26de5632a06cc9f1026e8
vonas <24623262+vonas@users.noreply.github.com>
2024-02-23 13:22:18 +0100

    Solve day 12 part 2

--- src/day12/solve.cpp
+++ src/day12/solve.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include <tuple>
 #include <algorithm>
 #include <cmath>
 #include <set>
@@ -39,21 +40,61 @@ namespace helper {
 		seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 	}
 
-	struct pair_hash {
-		std::size_t operator () (const std::pair<std::string, std::vector<uint32_t>>& p) const {
-			auto h1 = std::hash<std::string>{}(p.first);
-			auto h2 = uint32_vector_hasher{}(p.second);
-			hash_combine(h1, h2);
-			return h1;
+	using tuple_type = std::tuple<std::string, size_t, std::vector<uint32_t>, size_t>;
+
+	struct tuple_hash {
+		std::size_t operator () (const tuple_type& p) const {
+			auto hash = std::hash<std::string>{}(std::get<0>(p));
+			auto h2 = std::hash<size_t>{}(std::get<1>(p));
+			auto h3 = uint32_vector_hasher{}(std::get<2>(p));
+			auto h4 = std::hash<size_t>{}(std::get<3>(p));
+			hash_combine(hash, h2);
+			hash_combine(hash, h3);
+			hash_combine(hash, h4);
+			return hash;
 		}
 	};
 }
 
 struct Record
 {
+	static const size_t UNFOLD_AMOUNT = 5;
+
 	std::string pattern;
 	std::vector<uint32_t> sequence;
+
+	Record() {}
+
+	Record(std::string const& pattern, std::vector<uint32_t> const& sequence)
+		: pattern(pattern), sequence(sequence)
+	{}
+
+	Record unfolded() const
+	{
+		Record result;
+		std::ostringstream oss;
+		oss << pattern;
+		for (size_t i = 1; i < UNFOLD_AMOUNT; i++) {
+			oss << '?' << pattern;
+		}
+		result.pattern = oss.str();
+		result.sequence.reserve(UNFOLD_AMOUNT * sequence.size());
+		for (size_t i = 0; i < UNFOLD_AMOUNT; i++) {
+			result.sequence.insert(result.sequence.end(), sequence.begin(), sequence.end());
+		}
+		return result;
+	}
 };
+
+std::vector<Record> unfolded(std::vector<Record> const& records)
+{
+	std::vector<Record> result;
+	result.reserve(records.size());
+	for (auto const& record : records) {
+		result.push_back(record.unfolded());
+	}
+	return result;
+}
 
 Record parse_line(std::string const& line)
 {
@@ -81,23 +122,22 @@ std::vector<Record> parse_lines(std::vector<std::string> const& lines)
 	return result;
 }
 
-size_t solve(std::string const& pattern, std::vector<uint32_t> const& sequence, size_t offset)
+size_t solve(std::string const& pattern, size_t index, std::vector<uint32_t> const& sequence, size_t offset)
 {
-	// static std::unordered_map<std::pair<std::string, std::vector<uint32_t>>, size_t, helper::pair_hash> dp;
-
 	_ASSERT(offset < sequence.size());
 
-	/*auto args = std::make_pair(pattern, sequence);
+	static std::unordered_map<helper::tuple_type, size_t, helper::tuple_hash> dp;
+	auto args = std::make_tuple(pattern, index, sequence, offset);
 	if (dp.contains(args)) {
 		return dp[args];
-	}*/
+	}
 
 	uint32_t required = sequence[offset];
 	bool has_more = offset + 1 < sequence.size();
 
 	size_t sum = 0;
 
-	for (size_t i = 0; i < pattern.size(); i++) {
+	for (size_t i = index; i < pattern.size(); i++) {
 		char c = pattern[i];
 		_ASSERT(c == '?' || c == '.' || c == '#');
 
@@ -123,7 +163,7 @@ size_t solve(std::string const& pattern, std::vector<uint32_t> const& sequence, 
 			// add 1 since the next char must be '.'
 			size_t start_index = i + required + 1;
 			if (fits && start_index < pattern.size()) {
-				sum += solve(pattern.substr(start_index), sequence, offset + 1);
+				sum += solve(pattern, start_index, sequence, offset + 1);
 			}
 		}
 		else {
@@ -158,12 +198,14 @@ size_t solve(std::string const& pattern, std::vector<uint32_t> const& sequence, 
 		if (end_after) break;
 	}
 
+	dp[args] = sum;
+
 	return sum;
 }
 
 size_t solve(std::string const& pattern, std::vector<uint32_t> const& sequence)
 {
-	return solve(pattern, sequence, 0);
+	return solve(pattern, 0, sequence, 0);
 }
 
 size_t solve(Record const& record)
@@ -230,5 +272,31 @@ namespace Tests
 
 		auto lines = read_lines("./src/day12/input/in.txt");
 		EXPECT_EQ(7857, solve(parse_lines(lines)));
+	}
+
+	TEST(Record, Unfolded) {
+		auto const& r = Record(".#", { 1 }).unfolded();
+		EXPECT_EQ(r.pattern, ".#?.#?.#?.#?.#");
+		EXPECT_THAT(r.sequence, ElementsAre(1, 1, 1, 1, 1));
+	}
+
+	TEST(Solve, Part2Example1) {
+		auto lines = read_lines("./src/day12/input/p1e1.txt");
+		EXPECT_EQ(1, solve(parse_line(lines[0]).unfolded()));
+		EXPECT_EQ(16384, solve(parse_line(lines[1]).unfolded()));
+		EXPECT_EQ(1, solve(parse_line(lines[2]).unfolded()));
+		EXPECT_EQ(16, solve(parse_line(lines[3]).unfolded()));
+		EXPECT_EQ(2500, solve(parse_line(lines[4]).unfolded()));
+		EXPECT_EQ(506250, solve(parse_line(lines[5]).unfolded()));
+		EXPECT_EQ(525152, solve(unfolded(parse_lines(lines))));
+	}
+
+	TEST(Solve, Part2) {
+		// attempt 1: <no result>, no optimization
+		// attempt 2: 28606137449920, dynamic programming,
+		// especially effective since there are 5 identical copies of everything
+
+		auto lines = read_lines("./src/day12/input/in.txt");
+		EXPECT_EQ(28606137449920, solve(unfolded(parse_lines(lines))));
 	}
 }
