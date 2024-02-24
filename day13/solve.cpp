48a9451168d3ad3dba969328c57b4a4522d2ba2e
vonas <24623262+vonas@users.noreply.github.com>
2024-02-24 02:49:53 +0100

    Refactor

--- day13/solve.cpp
+++ day13/solve.cpp
@@ -158,161 +158,94 @@ struct ReflectionLine
 	bool is_horizontal = true; // horizontal: row reflection, vertical: col reflection
 	size_t index; // index of the row or col, depending on is_horizontal
 	std::optional<std::pair<size_t, size_t>> smudge;
+
+	size_t get_score() const
+	{
+		return is_horizontal ? index * 100 : index;
+	}
 };
 
-size_t solve2(Pattern const& input)
+void find_reflections(Pattern const& input, bool is_horizontal, std::vector<ReflectionLine>& out)
 {
-	// notes:
-	// - with loop 2, i'm not sure yet if it's entirely correct with the col calc.
-	// - we need to do bitmasks for the inverse too.. changing . to #
-	// - we don't have to check the inverse wtf
-
-	std::vector<ReflectionLine> reflections;
-
 	std::vector<uint64_t> prev_bitmasks;
 	size_t backtrack_index = 0;
 	size_t mirror_index = 0;
 	std::optional<std::pair<size_t, size_t>> smudge_position = std::nullopt;
 
-	for (size_t i = 0; i < input.lines.size(); i++) {
+	size_t outer_size = is_horizontal ? input.lines.size() : input.lines[0].size();
+	for (size_t i = 0; i < outer_size; i++) {
 		int64_t bitmask = 0;
-		for (size_t j = 0; j < input.lines[i].size(); j++) {
+		size_t inner_size = is_horizontal ? input.lines[i].size() : input.lines.size();
+		for (size_t j = 0; j < inner_size; j++) {
 			bitmask <<= 1;
-			bitmask |= input.lines[i][j] == '#' ? 1 : 0;
+			char value = is_horizontal ? input.lines[i][j] : input.lines[j][i];
+			bitmask |= value == '#' ? 1 : 0;
 		}
-		if (i > 0 && mirror_index == 0) { // && bitmask == prev_bitmasks.back()
+		if (i > 0 && mirror_index == 0) {
 			mirror_index = i;
 			backtrack_index = prev_bitmasks.size() - 1;
 			prev_bitmasks.push_back(bitmask);
 		}
-		if (mirror_index > 0) {
-			bool is_valid = bitmask == prev_bitmasks[backtrack_index];
-			if (!is_valid && !smudge_position.has_value()) {
-				auto smudges = bitmask ^ prev_bitmasks[backtrack_index];
-				// if the xor result is a power of 2, it's a single bit and thus a single smudge.
-				bool is_single_smudge = (smudges & (smudges - 1)) == 0;
-				if (is_single_smudge) {
-					is_valid = true;
-					size_t row = i;
-					if ((smudges & prev_bitmasks[backtrack_index]) > 0) {
-						// the smudge is in the backtrack bitmask
-						row = (mirror_index - 1) - (i - mirror_index);
-					}
-					size_t col = input.lines[0].size() - (size_t)std::log2(smudges) - 1;
-					smudge_position = std::make_pair(row, col);
+		if (mirror_index == 0) {
+			prev_bitmasks.push_back(bitmask);
+			continue;
+		}
+		bool is_valid = bitmask == prev_bitmasks[backtrack_index];
+		if (!is_valid && !smudge_position.has_value()) {
+			auto smudges = bitmask ^ prev_bitmasks[backtrack_index];
+			// if the xor result is a power of 2, it's a single bit and thus a single smudge.
+			bool is_single_smudge = (smudges & (smudges - 1)) == 0;
+			if (is_single_smudge) {
+				is_valid = true;
+				size_t a = i;
+				if ((smudges & prev_bitmasks[backtrack_index]) > 0) {
+					// the smudge is in the backtrack bitmask
+					a = (mirror_index - 1) - (i - mirror_index);
 				}
+				size_t b = outer_size - (size_t)std::log2(smudges) - 1;
+				smudge_position = is_horizontal ? std::make_pair(a, b) : std::make_pair(b, a);
 			}
-			if (is_valid) {
-				if (backtrack_index == 0 || i == input.lines.size() - 1) {
-					reflections.push_back(ReflectionLine{
-						.is_horizontal = true,
-						.index = mirror_index,
-						.smudge = smudge_position
-					});
-					i = mirror_index;
-					mirror_index = 0;
-					backtrack_index = 0;
-					smudge_position = std::nullopt;
-					continue;
-				}
-				backtrack_index--;
+		}
+		bool is_done = false;
+		if (is_valid) {
+			if (backtrack_index == 0 || i == outer_size - 1) {
+				out.push_back(ReflectionLine{
+					.is_horizontal = is_horizontal,
+					.index = mirror_index,
+					.smudge = smudge_position
+				});
+				is_done = true;
 			}
 			else {
-				// go back to where we left off
-				i = mirror_index;
-				mirror_index = 0;
-				backtrack_index = 0;
-				smudge_position = std::nullopt;
-			}
-		}
-		else {
-			prev_bitmasks.push_back(bitmask);
-		}
-	}
-
-	prev_bitmasks.clear();
-	backtrack_index = 0;
-	mirror_index = 0;
-	smudge_position = std::nullopt;
-
-	if (input.lines.size() == 9) {
-		int x = 0;
-	}
-
-	for (size_t j = 0; j < input.lines[0].size(); j++) {
-		int64_t bitmask = 0;
-		for (size_t i = 0; i < input.lines.size(); i++) {
-			bitmask <<= 1;
-			bitmask |= input.lines[i][j] == '#' ? 1 : 0;
-		}
-		if (j > 0 && mirror_index == 0) { // && bitmask == prev_bitmasks.back()
-			mirror_index = j;
-			backtrack_index = prev_bitmasks.size() - 1;
-			prev_bitmasks.push_back(bitmask);
-		}
-		if (mirror_index > 0) {
-			bool is_valid = bitmask == prev_bitmasks[backtrack_index];
-			if (!is_valid && !smudge_position.has_value()) {
-				auto smudges = bitmask ^ prev_bitmasks[backtrack_index];
-				// if the xor result is a power of 2, it's a single bit and thus a single smudge.
-				bool is_single_smudge = (smudges & (smudges - 1)) == 0;
-				if (is_single_smudge) {
-					is_valid = true;
-					size_t col = j;
-					if ((smudges & prev_bitmasks[backtrack_index]) > 0) {
-						// the smudge is in the backtrack bitmask
-						col = (mirror_index - 1) - (j - mirror_index);
-					}
-					smudge_position = std::make_pair(input.lines.size() - (size_t)std::log2(smudges), col);
-				}
-			}
-			if (is_valid) {
-				if (backtrack_index == 0 || j == input.lines[0].size() - 1) {
-					reflections.push_back(ReflectionLine{
-						.is_horizontal = false,
-						.index = mirror_index,
-						.smudge = smudge_position
-					});
-					j = mirror_index;
-					mirror_index = 0;
-					backtrack_index = 0;
-					smudge_position = std::nullopt;
-					continue;
-				}
 				backtrack_index--;
 			}
-			else {
-				// go back to where we left off
-				j = mirror_index;
-				mirror_index = 0;
-				backtrack_index = 0;
-				smudge_position = std::nullopt;
-			}
 		}
-		else {
-			prev_bitmasks.push_back(bitmask);
+		if (!is_valid || is_done) {
+			// go back to where we left off
+			i = mirror_index;
+			mirror_index = 0;
+			backtrack_index = 0;
+			smudge_position = std::nullopt;
 		}
 	}
+}
 
+size_t solve2(Pattern const& input)
+{
+	std::vector<ReflectionLine> reflections;
+	find_reflections(input, true, reflections);
+	find_reflections(input, false, reflections);
+
+	// must be one original reflection
+	// and one reflection with exactly one smudge.
 	_ASSERT(reflections.size() == 2);
 
-	auto reflection = reflections[0];
-	if (reflections[0].smudge.has_value()) {
-		reflection = reflections[0];
-	}
-	else if (reflections[1].smudge.has_value()) {
-		reflection = reflections[1];
-	}
+	auto const& it = std::find_if(reflections.begin(), reflections.end(), [](auto const& e) {
+		return e.smudge.has_value();
+	});
+	_ASSERT(it != reflections.end());
 
-	size_t result = 0;
-	if (reflection.is_horizontal) {
-		result = reflection.index * 100;
-	}
-	else {
-		result = reflection.index;
-	}
-
-	return result;
+	return it->get_score();
 }
 
 size_t solve2_all(std::vector<Pattern> patterns)
